19f96f5c65c7c0a9d68ab3a77d86b2fb275d1dc9
Madhav Verma <116595091+madhav-7781@users.noreply.github.com>
2024-08-02 03:36:45 +0530

    Create Logout.java

---
+++ Logout.java
@@ -0,0 +1,21 @@
+package com.login;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class Logout extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		HttpSession session = request.getSession();
+		session.removeAttribute("username");
+		session.invalidate();
+		response.sendRedirect("login.jsp");
+	}
+}
